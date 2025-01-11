fcf1f166eff481a36a5987a3966c10210fc9a777
Chao Zhang 张超 <zhangchaohci@gmail.com>
2025-01-11 22:11:45 +0800

    Add data

---
+++ lib/type.tsx
@@ -0,0 +1,60 @@
+export type Category =
+  | "claim"
+  | "reasoning"
+  | "evidence"
+  | "rebuttal"
+  | "organization"
+  | "word-usage"
+  | "orthography"
+  | "others";
+
+export type Sentence = {
+  id: number;
+  content: string;
+  paragraph: number;
+};
+
+export type HowItem = {
+  title: string;
+  strategy: string;
+};
+
+export type GPTPlan = {
+  sentence: string;
+  what: number[];
+  why: string;
+  how: HowItem[];
+};
+
+export type FeedbackItem = {
+  id: number;
+  content: string;
+  actionability: number;
+  justification: number;
+  sentiment: number;
+  specificity: number;
+  engagement: number;
+  source: number;
+  type: string;
+  file: string;
+  plan: GPTPlan[];
+  addressed: boolean;
+};
+
+export type FeedbackSourceItem = {
+  id: number;
+  content: string;
+  file: string;
+};
+
+export type RevisionItem = {
+  revision: string;
+  better: boolean;
+  explanation: string;
+};
+
+export type Revision = {
+  id: number;
+  sentence: string;
+  revision: RevisionItem[];
+};
