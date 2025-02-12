831d8c47ccbe92aa3df6ee946706571557652032
Chao Zhang 张超 <zhangchaohci@gmail.com>
2025-02-12 11:07:26 -0500

    Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,15 +10,17 @@ const Header = ({}: HeaderProps) => {
   return (
     <div className="flex flex-row w-full justify-between items-center p-4 border-b border-base-200 h-[3rem] bg-white absolute top-0 z-[100001]">
       <h1
-        className={noto_serif.className + " font-semibold text-md text-neutral"}
+        className={noto_serif.className + " font-semibold text-lg text-neutral"}
       >
         <Link href="/">Synthia</Link>
       </h1>
-      <Menu classes="absolute z-50 left-48" />
-      <TbInfoCircleFilled
-        size={24}
-        className="text-base-300 hover:text-neutral cursor-pointer"
-      />
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <Menu classes="z-50" />
+        <TbInfoCircleFilled
+          size={24}
+          className="text-base-300 hover:text-neutral cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
