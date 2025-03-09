d241880f0aaea642f5ef18f7a175048d110934af
Chao Zhang 张超 <zhangchaohci@gmail.com>
2025-03-08 21:41:41 -0500

    Update firebaseConfig.js

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2xRmLMZOFQ6VZS8bzEpreZcV1F0qLYOM",
-  authDomain: "synthia-11c9c.firebaseapp.com",
-  projectId: "synthia-11c9c",
-  storageBucket: "synthia-11c9c.firebasestorage.app",
-  messagingSenderId: "1035613486069",
-  appId: "1:1035613486069:web:4c1beb084634ae5d64febc",
-  measurementId: "G-H5WWE8KPJ8",
+  apiKey: "AIzaSyDKipHiEnrsKjI9aJwuur9a0rKesieA0to",
+  authDomain: "vivid-a6bd0.firebaseapp.com",
+  projectId: "vivid-a6bd0",
+  storageBucket: "vivid-a6bd0.firebasestorage.app",
+  messagingSenderId: "862164328335",
+  appId: "1:862164328335:web:36c2cfabe97270d29375fa",
+  measurementId: "G-6YWLGF4GCH",
 };
 
 // Initialize Firebase
